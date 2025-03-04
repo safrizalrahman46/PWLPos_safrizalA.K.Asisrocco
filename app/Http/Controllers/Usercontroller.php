014763cbe5a68197285d6948dd84de7a8a1b0eab
safrizalrahman46 <safrizalrahman46@gmail.com>
2025-03-04 20:22:38 +0700

    practicum 2.2

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -22,9 +22,13 @@ class UserController extends Controller
         // $user = UserModel::find(1); // Retrieve all users
         // $user = UserModel::where('level_id', 1)->first();
         // $user = UserModel::firstWhere('level_id', 1);
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        // practicum 2,2
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username','manager9')->firstOrFail();
         // Return the view with the user data
         return view('user', ['data' => $user]);
     }
