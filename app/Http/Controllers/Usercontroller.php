3adeb4c72dc231cfba3c6eb6a98cd3361e81f630
safrizalrahman46 <safrizalrahman46@gmail.com>
2025-03-04 20:13:19 +0700

    practicum 2.1

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -11,16 +11,20 @@ class UserController extends Controller
     public function index()
     {
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'manager 3',
-            'password' => Hash::make('12345')
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
 
-        UserModel::create($data);
-        $user = UserModel::all(); // Retrieve all users
-
+        // UserModel::create($data);
+        // $user = UserModel::find(1); // Retrieve all users
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         // Return the view with the user data
         return view('user', ['data' => $user]);
     }
--- resources/views/User.blade.php
+++ resources/views/User.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h1>Data User</h1>
-    <a href="{{ route('user.tambah') }}">Tambah User</a>
+    {{--  <a href="{{ route('user.tambah') }}">Tambah User</a>  --}}
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
@@ -13,14 +13,14 @@
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
-        @foreach ($data as $d)
+        {{--  @foreach ($data as $d)  --}}
         <tr>
-            <td>{{ $d->user_id }}</td>
-            <td>{{ $d->username }}</td>
-            <td>{{ $d->nama }}</td>
-            <td>{{ $d->level_id }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
-        @endforeach
+        {{--  @endforeach  --}}
     </table>
 </body>
 </html>
